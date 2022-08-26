1c6060e7edf8046cd5e02a3387904d68ad7a8a54
Tuomas Pöyry <me@tuomas.id>
2022-08-26 16:08:31 +0300

    Fix DB name

--- initialProject/lib/db.ts
+++ initialProject/lib/db.ts
@@ -7,7 +7,7 @@ export const conn = new Pool({
   password: process.env.POSTGRES_PASSWORD,
   host: process.env.POSTGRES_HOST,
   port: parseInt(process.env.POSTGRES_PORT!) || 5432,
-  database: process.env.POSTGRES_DATABASE,
+  database: process.env.POSTGRES_DB,
 });
 
 export const query = async <Row = any>(statement: string | SQLStatement) => {
